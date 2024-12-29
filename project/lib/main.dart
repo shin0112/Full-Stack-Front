7b4a335d37a9a9d1167436501786fea4b70af42b
shin0112 <wnslcosltimo12@gmail.com>
2024-12-30 04:23:57 +0900

    feat: 하단 네비 바 작성

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,125 +1,475 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:project/theme.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final _title = 'Coffhy';
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      color: Theme.of(context).colorScheme.surfaceDim,
+      title: _title,
+      theme: lightTheme,
+      home: const MyStatefulWidget(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyStatefulWidget> createState() => MyStatefulWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class Destination {
+  const Destination(this.index, this.title, this.icon);
+  final int index;
+  final String title;
+  final IconData icon;
+}
 
-  void _incrementCounter() {
+class MyStatefulWidgetState extends State<MyStatefulWidget> {
+  final PageController _pageController = PageController();
+
+  static const List<Destination> allDestinations = <Destination>[
+    Destination(0, 'Home', Icons.home_outlined),
+    Destination(1, 'Calendar', Icons.today),
+    Destination(2, 'Community', Icons.group),
+    Destination(3, 'Settings', Icons.settings),
+  ];
+
+  int _selectedIndex = 0;
+
+  final List<Widget> _widgetOptions = <Widget>[
+    HomeWidget(),
+    CalendarWidget(),
+    CommunityWidget(),
+    SettingWidget(),
+  ];
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void _onDestinationSelected(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Coffhy',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+        ),
+        body: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          children: _widgetOptions,
+        ),
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: _selectedIndex,
+          onDestinationSelected: _onDestinationSelected,
+          height: 80,
+          backgroundColor: Theme.of(context).colorScheme.surfaceContainerLowest,
+          indicatorColor: Theme.of(context).colorScheme.surfaceContainerHigh,
+          destinations: allDestinations.map<NavigationDestination>(
+            (Destination destination) {
+              return NavigationDestination(
+                selectedIcon: Icon(
+                  destination.icon,
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+                icon: Icon(
+                  destination.icon,
+                  color: Theme.of(context).colorScheme.onSecondaryContainer,
+                ),
+                label: destination.title,
+              );
+            },
+          ).toList(),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class SettingWidget extends StatelessWidget {
+  final Map info = {
+    'titleImageLink': 'https://storage.googleapis.com/cms-storage-bucket/'
+        '2f118a9971e4ca6ad737.png',
+    'titleSectionHeader': 'Flutter on Mobile',
+    'titleSectionBody': 'https://flutter.dev/multi-platform/mobile',
+    'titleSectionScore': 100,
+    'textSection': 'Bring your app idea to more users from day one by'
+        ' building with Flutter '
+        'on iOS and Android simultaneously, without sacrificing features, '
+        'quality, or performance. All mobile on day one: '
+        'Reach your full addressable market from day one by targeting users'
+        ' in both ecosystems from a single codebase. Do more with less: '
+        'Unite your mobile development team resources towards building '
+        'one seamless customer experience. One experience: '
+        'Release simultaneously on iOS and Android with feature parity '
+        'for the best experience for all users.',
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    final titleImage = _buildTitleImage(info['titleImageLink']);
+    Widget textSection = _buildTextSection(info['textSection']);
+    Widget buttonSection = _buildButtonSection(Theme.of(context).primaryColor);
+    Widget titleSection = _buildTitleSection(info['titleSectionHeader'],
+        info['titleSectionBody'], info['titleSectionScore']);
+
+    return Scaffold(
+      body: ListView(
+        children: [
+          titleImage,
+          titleSection,
+          buttonSection,
+          textSection,
+        ],
+      ),
+    );
+  }
+}
+
+// star widget : volume-E-chapter-07.dart
+
+class CommunityWidget extends StatelessWidget {
+  final Map info = {
+    'titleImageLink': 'https://storage.googleapis.com/cms-storage-bucket/'
+        '2f118a9971e4ca6ad737.png',
+    'titleSectionHeader': 'Flutter on Mobile',
+    'titleSectionBody': 'https://flutter.dev/multi-platform/mobile',
+    'titleSectionScore': 100,
+    'textSection': 'Bring your app idea to more users from day one by'
+        ' building with Flutter '
+        'on iOS and Android simultaneously, without sacrificing features, '
+        'quality, or performance. All mobile on day one: '
+        'Reach your full addressable market from day one by targeting users'
+        ' in both ecosystems from a single codebase. Do more with less: '
+        'Unite your mobile development team resources towards building '
+        'one seamless customer experience. One experience: '
+        'Release simultaneously on iOS and Android with feature parity '
+        'for the best experience for all users.',
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    final titleImage = _buildTitleImage(info['titleImageLink']);
+    Widget textSection = _buildTextSection(info['textSection']);
+    Widget buttonSection = _buildButtonSection(Theme.of(context).primaryColor);
+    Widget titleSection = _buildTitleSection(info['titleSectionHeader'],
+        info['titleSectionBody'], info['titleSectionScore']);
+
+    return Scaffold(
+      body: ListView(
+        children: [
+          titleImage,
+          titleSection,
+          buttonSection,
+          textSection,
+        ],
+      ),
+    );
+  }
+}
+
+Image _buildTitleImage(String imageName) {
+  return Image.network(
+    imageName,
+    width: 600,
+    height: 240,
+    fit: BoxFit.cover,
+  );
+}
+
+Container _buildTitleSection(String name, String addr, int count) {
+  return Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  name,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                addr,
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Counter(),
+      ],
+    ),
+  );
+}
+
+Widget _buildButtonSection(Color color) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      _buildButtonColumn(color, Icons.assistant_navigation, 'Visit'),
+      _buildButtonColumn(color, Icons.add_alert_sharp, 'Alarm'),
+      _buildButtonColumn(color, Icons.share, 'Share'),
+    ],
+  );
+}
+
+Column _buildButtonColumn(Color color, IconData icon, String label) {
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Icon(icon, color: color),
+      Container(
+        margin: const EdgeInsets.only(top: 8),
+        child: Text(
+          label,
+          style: TextStyle(
+            fontSize: 13,
+            fontWeight: FontWeight.w400,
+            color: color,
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+Container _buildTextSection(String section) {
+  return Container(
+    padding: const EdgeInsets.all(32),
+    child: Text(
+      section,
+      softWrap: true,
+      textAlign: TextAlign.justify,
+      style: const TextStyle(height: 1.5, fontSize: 15),
+    ),
+  );
+}
+
+class Counter extends StatefulWidget {
+  const Counter({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<Counter> createState() => CounterState();
+}
+
+class CounterState extends State<Counter> {
+  int _counter = 0;
+  bool _boolStatus = false;
+  Color _statusColor = Colors.black;
+
+  void _buttnPressed() {
+    setState(() {
+      if (_boolStatus == true) {
+        _boolStatus = false;
+        _counter--;
+        _statusColor = Colors.black;
+      } else {
+        _boolStatus = true;
+        _counter++;
+        _statusColor = Colors.red;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+          icon: const Icon(Icons.star),
+          color: _statusColor,
+          onPressed: _buttnPressed,
+        ),
+        Text('$_counter'),
+      ],
+    );
+  }
+}
+
+// hello widget : volume-E-chapter-02.dart
+
+class CalendarWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.black,
+            textStyle: const TextStyle(
+              fontSize: 32,
+            ),
+          ),
+          onPressed: () {
+            showAlertDialog(context);
+          },
+          child: const Text('Hello, Press Here!'),
+        ),
+      ),
+    );
+  }
+}
+
+void showAlertDialog(BuildContext context) async {
+  String result = await showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('AlertDialog Sample'),
+        content: const Text("Select button you want"),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('OK'),
+            onPressed: () {
+              Navigator.pop(context, "OK");
+            },
+          ),
+          TextButton(
+            child: const Text('Cancel'),
+            onPressed: () {
+              Navigator.pop(context, "Cancel");
+            },
+          ),
+        ],
+      );
+    },
+  );
+
+  print("showAlertDialog(): $result");
+}
+
+// home widget : volume-E-chapter-03-code-22.dart
+
+class HomeWidget extends StatelessWidget {
+  final groupAggregated = Container(
+    padding: const EdgeInsets.all(20),
+    child: Column(
+      children: [
+        const Text(
+          'Shop Name',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.black87,
+          ),
+        ),
+        Center(
+          child: Image.network(
+              'https://storage.googleapis.com/cms-storage-bucket/'
+              '780e0e64d323aad2cdd5.png',
+              width: 300,
+              height: 300),
+        ),
+        Container(
+          padding: const EdgeInsets.all(20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.star, color: Colors.green[500]),
+                  Icon(Icons.star, color: Colors.green[500]),
+                  const Icon(Icons.star, color: Colors.black),
+                ],
+              ),
+              const Text(
+                '170 Reviews',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w800,
+                  fontFamily: 'Roboto',
+                  letterSpacing: 0.5,
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+        DefaultTextStyle.merge(
+          style: const TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w800,
+            fontFamily: 'Roboto',
+            letterSpacing: 0.5,
+            fontSize: 18,
+          ),
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(Icons.kitchen, color: Colors.green[500]),
+                    const Text('kitchen:'),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(Icons.timer, color: Colors.green[500]),
+                    const Text('timer:'),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(Icons.restaurant, color: Colors.green[500]),
+                    const Text('restaurant:'),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        groupAggregated,
+      ]),
     );
   }
 }
