38b473da56b7737378d82bce17206a7e85ede6af
shin0112 <wnslcosltimo12@gmail.com>
2025-01-01 19:14:53 +0900

    feat: widget 애니메이션 삽입

--- project/lib/widgets/thin_bar.dart
+++ project/lib/widgets/thin_bar.dart
@@ -19,76 +19,70 @@ class ThinBar extends StatefulWidget {
 }
 
 class ThinBarState extends State<ThinBar> {
-  void _onTap() {}
-
   @override
   Widget build(BuildContext context) {
-    final content = GestureDetector(
-      onTap: _onTap,
-      child: Container(
-        alignment: Alignment.center,
-        padding: EdgeInsets.symmetric(horizontal: 12.sp),
+    final content = Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.symmetric(horizontal: 12.sp),
 
-        /// 내부 content
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            /// 좌측
-            Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.surfaceContainer,
-                    shape: BoxShape.circle,
-                  ),
-                  width: 35.sp,
-                  height: 35.sp,
-                  child: Icon(
-                    widget.icon,
-                    color: Theme.of(context).colorScheme.onSurface,
-                    size: 24.sp,
-                  ),
+      /// 내부 content
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          /// 좌측
+          Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surfaceContainer,
+                  shape: BoxShape.circle,
                 ),
-                SizedBox(width: 8.sp),
-                Text(
-                  widget.text,
-                  style: TextStyle(
-                    color: widget.brown
-                        ? Theme.of(context).colorScheme.surfaceContainer
-                        : Theme.of(context).colorScheme.onSurface,
-                    fontSize: 14.sp,
-                  ),
+                width: 35.sp,
+                height: 35.sp,
+                child: Icon(
+                  widget.icon,
+                  color: Theme.of(context).colorScheme.onSurface,
+                  size: 24.sp,
                 ),
-              ],
-            ),
+              ),
+              SizedBox(width: 8.sp),
+              Text(
+                widget.text,
+                style: TextStyle(
+                  color: widget.brown
+                      ? Theme.of(context).colorScheme.surfaceContainer
+                      : Theme.of(context).colorScheme.onSurface,
+                  fontSize: 14.sp,
+                ),
+              ),
+            ],
+          ),
 
-            /// 우측
-            Icon(
-              Icons.keyboard_arrow_right,
-              color: widget.brown
-                  ? Theme.of(context).colorScheme.onInverseSurface
-                  : Theme.of(context).colorScheme.onSurface,
-              size: 24.sp,
-            )
-          ],
-        ),
+          /// 우측
+          Icon(
+            Icons.keyboard_arrow_right,
+            color: widget.brown
+                ? Theme.of(context).colorScheme.onInverseSurface
+                : Theme.of(context).colorScheme.onSurface,
+            size: 24.sp,
+          )
+        ],
       ),
     );
 
-    return Container(
-      alignment: Alignment.topCenter,
-      decoration: BoxDecoration(
+    return SizedBox(
+      height: 55.sp,
+      child: Material(
         borderRadius: BorderRadius.circular(12.sp),
         color: widget.brown
             ? MaterialTheme.coffee.seed
             : Theme.of(context).colorScheme.onPrimary,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(12.sp),
+          onTap: () {},
+          child: content,
+        ),
       ),
-      height: 55.sp,
-      margin: EdgeInsets.symmetric(
-        horizontal: 16.sp,
-        vertical: 20.sp,
-      ),
-      child: content,
     );
   }
 }
