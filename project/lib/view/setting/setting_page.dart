04a1c631c7f8d6454ae103f4f47df0b326c78546
shin0112 <wnslcosltimo12@gmail.com>
2025-01-05 01:13:18 +0900

    feat: 빈 setting 페이지 생성

---
+++ project/lib/view/setting/setting_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SettingPage extends StatelessWidget {
+  const SettingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 620.sp,
+      width: 360.sp,
+      padding: EdgeInsets.symmetric(vertical: 20.sp, horizontal: 10.sp),
+    );
+  }
+}
