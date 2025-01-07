c276fc7cd64f0ee1ae5f1e1bb2691f16a69c1552
shin0112 <wnslcosltimo12@gmail.com>
2025-01-08 02:11:53 +0900

    feat: 선택 날짜 section 구현

--- project/lib/view/calendar/calendar_view.dart
+++ project/lib/view/calendar/calendar_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:project/config/themes/theme.dart';
 import 'package:project/view/calendar/calendar_view_model.dart';
+import 'package:project/widgets/line.dart';
 import 'package:provider/provider.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -15,6 +16,9 @@ class CalendarView extends StatelessWidget {
         children: [
           _buildCalendar(context, provider),
           SizedBox(height: 10.sp),
+          HorizontalLine(width: 320.sp),
+          SizedBox(height: 10.sp),
+          _buildDateSection(context, provider.selectedDay),
         ],
       );
     });
@@ -44,6 +48,28 @@ class CalendarView extends StatelessWidget {
           shape: BoxShape.circle,
         ),
       ),
+    );
+  }
+
+  Widget _buildDateSection(BuildContext context, DateTime selectedDay,
+      [double caffeineSum = 0.0]) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          "${selectedDay.year}년 ${selectedDay.month}월 ${selectedDay.day}일",
+          style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                fontSize: 16.sp,
+              ),
+        ),
+        Text(
+          "${caffeineSum}mg",
+          style: Theme.of(context)
+              .textTheme
+              .titleMedium
+              ?.copyWith(fontSize: 16.sp),
+        ),
+      ],
     );
   }
 }
