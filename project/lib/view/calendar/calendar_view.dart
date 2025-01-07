4d41749e514e102435f1af626899885c3ebf3073
shin0112 <wnslcosltimo12@gmail.com>
2025-01-08 01:21:29 +0900

    feat: 캘린더 삽입 및 파라미터 조정

--- project/lib/view/calendar/calendar_view.dart
+++ project/lib/view/calendar/calendar_view.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:project/view/calendar/calendar_view_modal.dart';
+import 'package:provider/provider.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarView extends StatelessWidget {
   const CalendarView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Consumer<CalendarViewModal>(builder: (context, provider, child) {
+      return TableCalendar(
+        focusedDay: provider.focusedDay,
+        firstDay: DateTime.utc(2024, 1, 1),
+        lastDay: DateTime.utc(2025, 12, 31),
+        selectedDayPredicate: (day) => isSameDay(provider.selectedDay, day),
+        onDaySelected: (selectedDay, focusedDay) =>
+            provider.onDaySelected(focusedDay, selectedDay),
+        calendarFormat: provider.calendarFormat,
+        onFormatChanged: (format) => provider.onFormatChanged(format),
+        onPageChanged: (focusedDay) => provider.onFocusedDayChanged(focusedDay),
+      );
+    });
   }
 }
--- project/lib/view/calendar/calendar_view_modal.dart
+++ project/lib/view/calendar/calendar_view_modal.dart
@@ -1,5 +1,35 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import 'package:project/data/model/record.dart';
+import 'package:project/utils/get_hash_code.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarViewModal with ChangeNotifier {
+  DateTime get focusedDay => _focusedDay;
+  DateTime _focusedDay = DateTime.now();
+
+  DateTime get selectedDay => _selectedDay;
+  DateTime _selectedDay = DateTime.now();
+
+  CalendarFormat get calendarFormat => _calendarFormat;
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+
+  void onDaySelected(DateTime focusedDay, DateTime selectedDay) {
+    // _focusedDay = focusedDay;
+    _selectedDay = selectedDay;
+    notifyListeners();
+  }
+
+  void onFocusedDayChanged(DateTime focusedDay) {
+    _focusedDay = focusedDay;
+    notifyListeners();
+  }
+
+  void onFormatChanged(CalendarFormat format) {
+    _calendarFormat = format;
+    notifyListeners();
+  }
+
   CalendarViewModal();
 }
