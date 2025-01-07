a2e3f402112ea8ba905c2b8d25a7e6eac65a0734
shin0112 <wnslcosltimo12@gmail.com>
2025-01-07 18:59:05 +0900

    feat: 브랜드 리스트 틀 잡기
    - brand 값 가지고 와서 list view 작성

--- project/lib/view/home/beverage_view.dart
+++ project/lib/view/home/beverage_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:project/config/themes/theme.dart';
+import 'package:project/data/model/brand.dart';
+import 'package:project/view/home/beverage_view_model.dart';
+import 'package:provider/provider.dart';
 
 class BeverageView extends StatefulWidget {
   const BeverageView({super.key});
@@ -9,19 +13,98 @@ class BeverageView extends StatefulWidget {
 }
 
 class BeverageViewState extends State<BeverageView> {
+  int _selectedId = 0;
+
+  void selectId(int id) {
+    _selectedId = id;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 670.sp,
-      child: Row(
-        children: [_buildLeftList(context)],
+    return Consumer<BeverageViewModel>(
+      builder: (context, provider, child) => SizedBox(
+        height: 670.sp,
+        child: Row(
+          children: [_buildLeftList(context, provider.brandList)],
+        ),
       ),
     );
   }
 
-  Widget _buildLeftList(BuildContext context) {
-    return Column(
-      children: [Text("전체")],
+  Widget _buildLeftList(BuildContext context, List<List<Brand>> brandList) {
+    return SizedBox(
+      width: 80.sp,
+      height: 670.sp,
+      child: Column(
+        children: [
+          Text(
+            "전체",
+            style: Theme.of(context)
+                .textTheme
+                .bodyMedium!
+                .copyWith(fontSize: 14.sp),
+          ),
+          ListView(
+              children: brandList.map((brand) {
+            return Column(
+                children: brand
+                    .map((item) => _buildTextBox(
+                          item.name,
+                          item.id,
+                          item.id == _selectedId ? 1 : 0,
+                        ))
+                    .toList());
+          }).toList()),
+        ],
+      ),
     );
+  }
+
+  Widget _buildTextBox(String text, int id, [int type = 0]) {
+    TextStyle defaultTextStyle =
+        Theme.of(context).textTheme.bodyMedium!.copyWith(fontSize: 14.sp);
+
+    switch (type) {
+      // default
+      case 0:
+        return GestureDetector(
+          onTap: () => selectId(id),
+          child: Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.symmetric(vertical: 8.sp),
+            child: Text(text, style: defaultTextStyle),
+          ),
+        );
+      // selected
+      case 1:
+        return Container(
+          alignment: Alignment.center,
+          color: Theme.of(context).colorScheme.secondaryContainer,
+          padding: EdgeInsets.symmetric(vertical: 8.sp),
+          child: Text(text, style: defaultTextStyle),
+        );
+      // category
+      case 2:
+        return Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.symmetric(vertical: 8.sp),
+          decoration: BoxDecoration(
+            color: MaterialTheme.coffee.seed,
+            border: Border.symmetric(
+              horizontal: BorderSide(
+                width: 1,
+                color: Theme.of(context).colorScheme.outline,
+              ),
+            ),
+          ),
+          child: Text(
+            text,
+            style: defaultTextStyle.copyWith(
+                color: Theme.of(context).colorScheme.onSecondary),
+          ),
+        );
+      default:
+        return const SizedBox.shrink();
+    }
   }
 }
