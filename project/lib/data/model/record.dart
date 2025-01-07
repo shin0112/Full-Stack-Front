64bd208b0a523e0bdc0aa9aa525b63ee7d72adaf
shin0112 <wnslcosltimo12@gmail.com>
2025-01-08 01:38:05 +0900

    feat: record model, repository 파일 작성

---
+++ project/lib/data/model/record.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+class Record {
+  int id;
+  int userId;
+  double caffeine;
+  String title;
+  String detail;
+  String createdAt;
+
+  Record({
+    required this.id,
+    required this.userId,
+    required this.caffeine,
+    required this.title,
+    required this.detail,
+    required this.createdAt,
+  });
+
+  factory Record.fromJson(Map<String, dynamic> json) {
+    return Record(
+      id: json["id"] as int,
+      userId: json["userId"] as int,
+      caffeine: json["caffeine"] as double,
+      title: json["title"] as String,
+      detail: json["detail"] as String,
+      createdAt: json["createdAt"] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'userId': userId,
+      'caffeine': caffeine,
+      'title': title,
+      'detail': detail,
+      'createdAt': createdAt,
+    };
+  }
+}
+
+Future<List<Record>> parseRecordFromJson(String jsonString) async {
+  final List<dynamic> jsonData = json.decode(jsonString);
+  return jsonData.map((item) => Record.fromJson(item)).toList();
+}
