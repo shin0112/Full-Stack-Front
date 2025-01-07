1b936cf6c294bff7e57c17dddca0f4ad27e54173
shin0112 <wnslcosltimo12@gmail.com>
2025-01-08 00:07:44 +0900

    feat: caffeine view 작성 및 view model 연결

--- project/lib/view/home/caffeine_view.dart
+++ project/lib/view/home/caffeine_view.dart
@@ -5,7 +5,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gif/gif.dart';
 import 'package:intl/intl.dart';
 import 'package:project/config/themes/theme.dart';
+import 'package:project/view/home/caffeine_view_modal.dart';
 import 'package:project/widgets/line.dart';
+import 'package:provider/provider.dart';
 
 class TodayCaffeineView extends StatefulWidget {
   const TodayCaffeineView({super.key});
@@ -14,11 +16,7 @@ class TodayCaffeineView extends StatefulWidget {
   State<StatefulWidget> createState() => TodayCaffeineViewState();
 }
 
-// gif, 날짜, text, today 섭취한 카페인mg/ 개인마다 다른 카페인mg
 class TodayCaffeineViewState extends State<TodayCaffeineView> {
-  int todayCaffeine = 400;
-  final int limitCaffeine = 400;
-
   String date = DateFormat("yyyy년 MM월 dd일").format(DateTime.now());
   late Timer _midnightTimer;
 
@@ -31,16 +29,9 @@ class TodayCaffeineViewState extends State<TodayCaffeineView> {
 
     _midnightTimer = Timer(durationUntilMidnight, () {
       setState(() {
-        todayCaffeine = 0;
         date = DateFormat("yyyy년 MM월 dd일").format(DateTime.now());
       });
       _setMidnightResetTimer();
-    });
-  }
-
-  void addCaffeine(int amount) {
-    setState(() {
-      todayCaffeine += amount;
     });
   }
 
@@ -52,77 +43,86 @@ class TodayCaffeineViewState extends State<TodayCaffeineView> {
 
   @override
   Widget build(BuildContext context) {
-    double progressRatio = todayCaffeine / limitCaffeine;
-    progressRatio = progressRatio > 1.0 ? 1.0 : progressRatio;
+    return Consumer<CaffeineViewModal>(
+      builder: (context, provider, child) {
+        double progressRatio = provider.todayCaffeine / provider.limitCaffeine;
+        progressRatio = progressRatio > 1.0 ? 1.0 : progressRatio;
 
-    return Container(
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.onSecondary,
-        borderRadius: BorderRadius.circular(12.sp),
-      ),
-      height: 160.sp,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          // 좌측
-          Container(
-              width: 22.sp,
-              height: 160.sp,
-              decoration: BoxDecoration(
-                border: Border(
-                  right: BorderSide(
-                    color:
-                        Theme.of(context).colorScheme.surfaceContainerHighest,
-                    width: 1.0,
-                    style: BorderStyle.solid,
+        return Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.onSecondary,
+            borderRadius: BorderRadius.circular(12.sp),
+          ),
+          height: 160.sp,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // 좌측
+              Container(
+                  width: 22.sp,
+                  height: 160.sp,
+                  decoration: BoxDecoration(
+                    border: Border(
+                      right: BorderSide(
+                        color: Theme.of(context)
+                            .colorScheme
+                            .surfaceContainerHighest,
+                        width: 1.0,
+                        style: BorderStyle.solid,
+                      ),
+                    ),
                   ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      if (progressRatio < 1.0 && provider.todayCaffeine > 0)
+                        Gif(
+                          autostart: Autostart.loop,
+                          image: const AssetImage("assets/images/curve.gif"),
+                        ),
+                      AnimatedContainer(
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.easeInOut,
+                        height: 160.sp * progressRatio,
+                        decoration: BoxDecoration(
+                          color: MaterialTheme.coffee.seed,
+                          borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(12),
+                              bottomLeft: Radius.circular(12)),
+                        ),
+                      ),
+                    ],
+                  )),
+
+              // 우측
+              Container(
+                padding: EdgeInsets.only(
+                  left: 18.sp,
+                  right: 18.sp,
+                  top: 23.sp,
+                  bottom: 2.sp,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    // 상단 text container
+                    _buildTopTextSection(context),
+                    SizedBox(height: 4.sp),
+                    const HorizontalLine(width: 282),
+                    SizedBox(height: 4.sp),
+                    // 카페인 text
+                    _buildCaffeineSection(
+                      context,
+                      provider.todayCaffeine.toInt(),
+                      provider.limitCaffeine.toInt(),
+                    ),
+                  ],
                 ),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  if (progressRatio < 1.0 && todayCaffeine > 0)
-                    Gif(
-                      autostart: Autostart.loop,
-                      image: const AssetImage("assets/images/curve.gif"),
-                    ),
-                  AnimatedContainer(
-                    duration: const Duration(milliseconds: 500),
-                    curve: Curves.easeInOut,
-                    height: 160.sp * progressRatio,
-                    decoration: BoxDecoration(
-                      color: MaterialTheme.coffee.seed,
-                      borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(12),
-                          bottomLeft: Radius.circular(12)),
-                    ),
-                  ),
-                ],
-              )),
-
-          // 우측
-          Container(
-            padding: EdgeInsets.only(
-              left: 18.sp,
-              right: 18.sp,
-              top: 23.sp,
-              bottom: 2.sp,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                // 상단 text container
-                _buildTopTextSection(context),
-                SizedBox(height: 4.sp),
-                const HorizontalLine(width: 282),
-                SizedBox(height: 4.sp),
-                // 카페인 text
-                _buildCaffeineSection(context),
-              ],
-            ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 
@@ -147,7 +147,11 @@ class TodayCaffeineViewState extends State<TodayCaffeineView> {
     );
   }
 
-  Widget _buildCaffeineSection(BuildContext context) {
+  Widget _buildCaffeineSection(
+    BuildContext context,
+    int todayCaffeine,
+    int limitCaffeine,
+  ) {
     TextStyle todayCaffeineTextStyle = TextStyle(
       color: Theme.of(context).colorScheme.onSurface,
       fontSize: 64.sp,
