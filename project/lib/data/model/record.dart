8f8afccc5d3e18f5259ba6b838ae92aeb46990c7
shin0112 <wnslcosltimo12@gmail.com>
2025-01-10 17:50:04 +0900

    refactor: 기록 model 변경
    - id nullable
    - 불필요한 함수 제거
    - toMap, fromMap 구현

--- project/lib/data/model/record.dart
+++ project/lib/data/model/record.dart
@@ -1,7 +1,5 @@
-import 'dart:convert';
-
 class Record {
-  int id;
+  int? id;
   int? brandId;
   double caffeine;
   String title;
@@ -9,7 +7,7 @@ class Record {
   DateTime createdAt;
 
   Record({
-    required this.id,
+    this.id,
     this.brandId,
     required this.caffeine,
     required this.title,
@@ -17,30 +15,29 @@ class Record {
     required this.createdAt,
   });
 
-  factory Record.fromJson(Map<String, dynamic> json) {
-    return Record(
-      id: json["id"] as int,
-      brandId: json["brandId"] as int?,
-      caffeine: json["caffeine"] as double,
-      title: json["title"] as String,
-      detail: json["detail"] as String,
-      createdAt: DateTime.parse(json["createdAt"] as String),
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
+  Map<String, Object?> toMap() {
+    final map = <String, Object?>{
       'brandId': brandId,
       'caffeine': caffeine,
       'title': title,
       'detail': detail,
       'createdAt': createdAt,
     };
-  }
-}
 
-Future<List<Record>> parseRecordFromJson(String jsonString) async {
-  final List<dynamic> jsonData = json.decode(jsonString);
-  return jsonData.map((item) => Record.fromJson(item)).toList();
+    if (id != null) {
+      map['id'] = id;
+    }
+    return map;
+  }
+
+  factory Record.fromMap(Map<String, Object?> map) {
+    return Record(
+      id: map["id"] as int,
+      brandId: map["brandId"] as int?,
+      caffeine: map["caffeine"] as double,
+      title: map["title"] as String,
+      detail: map["detail"] as String,
+      createdAt: DateTime.parse(map["createdAt"] as String),
+    );
+  }
 }
