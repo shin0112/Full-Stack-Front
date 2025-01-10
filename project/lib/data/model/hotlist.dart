975e99d6eb029f55919748ba6a12d37513640398
shin0112 <wnslcosltimo12@gmail.com>
2025-01-10 16:38:40 +0900

    feat: 즐겨찾기 toMap, fromMap 함수 제작

--- project/lib/data/model/hotlist.dart
+++ project/lib/data/model/hotlist.dart
@@ -13,26 +13,28 @@ class Hotlist {
     required this.caffeine,
   });
 
-  factory Hotlist.fromJson(Map<String, dynamic> json) {
-    return Hotlist(
-      id: json['id'] as int,
-      name: json['name'] as String,
-      detail: json['detail'] as String,
-      caffeine: (json['caffeine'] as int).toDouble() / 10,
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
+  Map<String, Object?> toMap() {
+    final map = <String, Object?>{
       'id': id,
       'name': name,
       'detail': detail,
-      'caffeine': caffeine * 10,
+      'caffeine': caffeine,
     };
+
+    return map;
+  }
+
+  factory Hotlist.fromMap(Map<String, Object?> map) {
+    return Hotlist(
+      id: map['id'] as int,
+      name: map['name'] as String,
+      detail: map['detail'] as String,
+      caffeine: map['caffeine'] as double,
+    );
   }
 }
 
 Future<List<Hotlist>> parseHotlistFromJson(String jsonString) async {
   final List<dynamic> jsonData = json.decode(jsonString);
-  return jsonData.map((item) => Hotlist.fromJson(item)).toList();
+  return jsonData.map((item) => Hotlist.fromMap(item)).toList();
 }
