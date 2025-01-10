f433b3b71e626583dbf2669464e649841b181618
shin0112 <wnslcosltimo12@gmail.com>
2025-01-10 21:49:44 +0900

    refactor: 타이머 코드 view model로 이동

--- project/lib/view/home/caffeine_view_modal.dart
+++ project/lib/view/home/caffeine_view_modal.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class CaffeineViewModal with ChangeNotifier {
   double get todayCaffeine => _todayCaffeine;
@@ -6,6 +9,24 @@ class CaffeineViewModal with ChangeNotifier {
 
   double get limitCaffeine => _limitCaffeine;
   final double _limitCaffeine = 400;
+
+  String get date => _date;
+  String _date = DateFormat("yyyy년 MM월 dd일").format(DateTime.now());
+
+  late Timer _midnightTimer;
+
+  // todo: 나이, 키, 몸무게로 적정 카페인 구하기 함수
+
+  void _setMidnightResetTimer() {
+    final now = DateTime.now();
+    final midnight = DateTime(now.year, now.month, now.day + 1);
+    final durationUntilMidnight = midnight.difference(now);
+
+    _midnightTimer = Timer(durationUntilMidnight, () {
+      _date = DateFormat("yyyy년 MM월 dd일").format(DateTime.now());
+      _setMidnightResetTimer();
+    });
+  }
 
   num getLimitCaffeine(double weight) {
     const double maxCaffeinePerKg = 6.0;
@@ -17,5 +38,7 @@ class CaffeineViewModal with ChangeNotifier {
     notifyListeners();
   }
 
-  CaffeineViewModal();
+  CaffeineViewModal() {
+    _setMidnightResetTimer();
+  }
 }
