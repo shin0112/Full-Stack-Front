97369cc5bd3dbc06152c05eba944933e881b1504
shin0112 <wnslcosltimo12@gmail.com>
2025-01-10 21:58:52 +0900

    chore: 주석 위치 변경

--- project/lib/view/home/caffeine_view_modal.dart
+++ project/lib/view/home/caffeine_view_modal.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+// todo: 나이, 키, 몸무게로 적정 카페인 구하기 함수
 class CaffeineViewModal with ChangeNotifier {
   double get todayCaffeine => _todayCaffeine;
   double _todayCaffeine = 0;
@@ -15,7 +16,6 @@ class CaffeineViewModal with ChangeNotifier {
 
   late Timer _midnightTimer;
 
-  // todo: 나이, 키, 몸무게로 적정 카페인 구하기 함수
   CaffeineViewModal() {
     _setMidnightResetTimer();
   }
