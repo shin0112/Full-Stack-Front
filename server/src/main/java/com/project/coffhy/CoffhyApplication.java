11a44ec83c518f09c476a85ccf8b501b7a4046c5
shin0112 <wnslcosltimo12@gmail.com>
2025-01-11 22:10:33 +0900

    chore: 서버 초기 설정

---
+++ server/src/main/java/com/project/coffhy/CoffhyApplication.java
@@ -0,0 +1,13 @@
+package com.project.coffhy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffhyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffhyApplication.class, args);
+	}
+
+}
