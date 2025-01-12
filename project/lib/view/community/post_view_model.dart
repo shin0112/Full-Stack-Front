ed3ebb2239dcf1fadc0612baad6d1cc887ad1e9d
shin0112 <wnslcosltimo12@gmail.com>
2025-01-13 04:02:10 +0900

    refactor: class 위치 이동

--- project/lib/view/community/post_view_model.dart
+++ project/lib/view/community/post_view_model.dart
@@ -2,11 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:project/data/model/post.dart';
 import 'package:project/data/repository/post_repository.dart';
 
+class PostMode {
+  const PostMode(this.mode, this.title);
+  final int mode;
+  final String title;
+}
+
 class PostViewModel with ChangeNotifier {
   final PostRepository _postRepository = PostRepository();
 
   List<Post> get items => _items;
   List<Post> _items = [];
+
+  List<Post> get myPostList => _myPostList;
+  List<Post> _myPostList = [];
+
+  PostMode get mode => _mode;
+  PostMode _mode = postModeList[0];
+
+  static const List<PostMode> postModeList = <PostMode>[
+    PostMode(0, '전체'),
+    PostMode(1, 'MY'),
+  ];
 
   PostViewModel() {
     _fetchData();
@@ -14,6 +31,18 @@ class PostViewModel with ChangeNotifier {
 
   Future<void> _fetchData() async {
     _items = await _postRepository.fetchData();
+    notifyListeners();
+  }
+
+  Future<void> getMyPostList() async {
+    if (_myPostList.isEmpty) {
+      _myPostList = await _postRepository.getMine();
+    }
+    notifyListeners();
+  }
+
+  void changeMode(int mode) {
+    _mode = postModeList[mode];
     notifyListeners();
   }
 }
