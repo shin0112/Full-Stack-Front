a2a7f3e1519684d6a61ffb9cb9541814f246b916
shin0112 <wnslcosltimo12@gmail.com>
2025-01-13 04:01:28 +0900

    feat: 내 글 가져오기 함수 작성

--- project/lib/data/datasource/post_datasource.dart
+++ project/lib/data/datasource/post_datasource.dart
@@ -46,6 +46,41 @@ class PostDatasource {
     return postList;
   }
 
+  Future<List<Post>> findMine() async {
+    var userBox = await HiveDatabase().userBox;
+    var userId = userBox.getAt(0)!.id;
+
+    List<Post> postList = [];
+
+    try {
+      final uri = Uri.parse("$baseUrl/posts/mine/$userId");
+      final response = await http.get(uri).timeout(const Duration(seconds: 5));
+
+      if (response.statusCode == 200) {
+        final String responseBody = utf8.decode(response.bodyBytes);
+        log("디코딩: $responseBody");
+
+        final Map<String, dynamic> json = jsonDecode(responseBody);
+        log("서버 응답: $json");
+
+        try {
+          postList = await parsePostFromJson(json['data']);
+          log("파싱 데이터: $postList");
+        } catch (e) {
+          log("게시물 데이터 파싱 중 오류 발생: ${e.toString()}");
+          rethrow;
+        }
+      } else {
+        log("호출 실패: 상태 코드 ${response.statusCode}, 응답 내용: ${response.body}");
+      }
+    } catch (e) {
+      log("findAll 오류 발생: ${e.toString()}");
+      rethrow;
+    }
+
+    return postList;
+  }
+
   Future<void> create(String title, String content) async {
     var userBox = await HiveDatabase().userBox;
     var userId = userBox.getAt(0)!.id;
