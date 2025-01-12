202bc364d780d1ec0185fc605defcab14846a15d
shin0112 <wnslcosltimo12@gmail.com>
2025-01-13 04:01:49 +0900

    feat: 내 글 가져오기 data source, repository 연결

--- project/lib/data/repository/post_repository.dart
+++ project/lib/data/repository/post_repository.dart
@@ -2,12 +2,15 @@ import 'package:project/data/datasource/post_datasource.dart';
 import 'package:project/data/model/post.dart';
 
 class PostRepository {
-  PostDatasource _postDatasource = PostDatasource();
+  final PostDatasource _postDatasource = PostDatasource();
 
   PostRepository();
 
-  // todo: 실제 데이터 연결하기
   Future<List<Post>> fetchData() async {
     return await _postDatasource.findAll();
+  }
+
+  Future<List<Post>> getMine() async {
+    return await _postDatasource.findMine();
   }
 }
