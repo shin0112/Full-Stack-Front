e0736893e210585e299f5379b42c04f94610d170
shin0112 <wnslcosltimo12@gmail.com>
2025-01-13 03:43:30 +0900

    feat: data socuce와 연결

--- project/lib/data/repository/post_repository.dart
+++ project/lib/data/repository/post_repository.dart
@@ -1,13 +1,13 @@
-import 'package:flutter/services.dart';
+import 'package:project/data/datasource/post_datasource.dart';
 import 'package:project/data/model/post.dart';
 
 class PostRepository {
+  PostDatasource _postDatasource = PostDatasource();
+
   PostRepository();
 
   // todo: 실제 데이터 연결하기
   Future<List<Post>> fetchData() async {
-    final String jsonString =
-        await rootBundle.loadString('assets/dummy/post.json');
-    return parsePostFromJson(jsonString);
+    return await _postDatasource.findAll();
   }
 }
